8781492c8d62f9928f3c18099862d041a6e4aa6e
Galiev <ggvalion@gmail.com>
2023-04-25 12:25:35 +0500

    Init

---
+++ src/main/java/dev/galiev/rt_lib/RadiusToolLib.java
@@ -0,0 +1,14 @@
+package dev.galiev.rt_lib;
+
+import com.mojang.logging.LogUtils;
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+
+public class RadiusToolLib implements ModInitializer {
+    public static final String MOD_ID = "rt_lib";
+    public static final Logger LOGGER = LogUtils.getLogger();
+    @Override
+    public void onInitialize() {
+        LOGGER.info(MOD_ID + "loaded");
+    }
+}
