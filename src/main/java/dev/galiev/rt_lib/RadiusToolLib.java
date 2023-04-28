08f988ec587f99a4ccd70f2a7aeebdb12171782a
Galiev <ggvalion@gmail.com>
2023-04-28 17:34:26 +0500

    Radius Sword Completed

--- src/main/java/dev/galiev/rt_lib/RadiusToolLib.java
+++ src/main/java/dev/galiev/rt_lib/RadiusToolLib.java
@@ -1,31 +1,15 @@
 package dev.galiev.rt_lib;
 
 import com.mojang.logging.LogUtils;
-import dev.galiev.rt_lib.items.tools.RadiusAxe;
-import dev.galiev.rt_lib.items.tools.RadiusHoe;
-import dev.galiev.rt_lib.items.tools.RadiusPickaxe;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.minecraft.item.ToolMaterials;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
-import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 
 public class RadiusToolLib implements ModInitializer {
     public static final String MOD_ID = "rt_lib";
     public static final Logger LOGGER = LogUtils.getLogger();
 
-    public static final RadiusPickaxe RADIUS_PICKAXE = new RadiusPickaxe(ToolMaterials.NETHERITE, 1, 1.0F, new FabricItemSettings(), 1);
-    public static final RadiusHoe RADIUS_HOE = new RadiusHoe(ToolMaterials.NETHERITE, 1, 1.0F, new FabricItemSettings(), 1);
-    public static final RadiusAxe RADIUS_AXE = new RadiusAxe(ToolMaterials.NETHERITE,1.0F, 1.0F, new FabricItemSettings(), 1);
-
     @Override
     public void onInitialize() {
-        LOGGER.info(MOD_ID + "loaded");
-
-        Registry.register(Registries.ITEM, new Identifier(MOD_ID,"radius_pickaxe"), RADIUS_PICKAXE);
-        Registry.register(Registries.ITEM, new Identifier(MOD_ID,"radius_hoe"), RADIUS_HOE);
-        Registry.register(Registries.ITEM, new Identifier(MOD_ID,"radius_axe"), RADIUS_AXE);
+        LOGGER.info(MOD_ID + "Loaded");
     }
 }
---
+++ src/main/java/dev/galiev/rt_lib/items/tools/RadiusSword.java
@@ -0,0 +1,38 @@
+package dev.galiev.rt_lib.items.tools;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.SwordItem;
+import net.minecraft.item.ToolMaterial;
+import net.minecraft.util.math.Box;
+
+import java.util.List;
+
+public class RadiusSword extends SwordItem {
+    private int radius = 1;
+    private int radiusDamage = 1;
+    public RadiusSword(ToolMaterial toolMaterial, int attackDamage, float attackSpeed, Settings settings, int radius, int radiusDamage) {
+        super(toolMaterial, attackDamage, attackSpeed, settings);
+        this.radius = radius;
+        this.radiusDamage = radiusDamage;
+    }
+
+    @Override
+    public boolean postHit(ItemStack stack, LivingEntity target, LivingEntity attacker) {
+        if (attacker instanceof PlayerEntity player) {
+            var world = player.getWorld();
+            var blockPos = player.getBlockPos();
+
+            List<Entity> entities = world.getEntitiesByClass(Entity.class, new Box(blockPos).expand(radius), entity ->
+                    entity instanceof LivingEntity && entity != attacker);
+
+            for (var entity : entities){
+                entity.damage(world.getDamageSources().playerAttack(player), radiusDamage);
+            }
+            return true;
+        }
+        return false;
+    }
+}
